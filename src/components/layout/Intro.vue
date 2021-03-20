e61f2f1a8f228b7a1aea9fc0b82a211e320e357a
Arjun Kalburgi <askalburgi@gmail.com>
2021-03-20 00:58:25 -0400

    show text transition effect

--- src/components/layout/Intro.vue
+++ src/components/layout/Intro.vue
@@ -7,18 +7,18 @@
         Robin Mazumder
       </h2>
       <p class="intro_text intro_text--para">
-        Life is about relationships. Relationships with ourselves, others and our world. When I realized how much impact the places we live have on our ability to build and keep these relationships, a light went on in my head. If we can build better environments for our cities and our people, we can dramatically improve our mental and physical health, our friendships and families, our communities, and of course, our relationships. 
+        Life is about relationships. Relationships with ourselves, others and our world. When I realized how much impact the places we live have on our ability to build and keep these relationships, a light went on in my head. If we can build better environments for our cities and our people, we can dramatically improve our mental and physical health, our friendships and families, our communities, and of course, our relationships. <span v-if="!showText"><button class="intro_button" v-on:click="showText = !showText">read on</button></span>
       </p>
 
-      <p class="intro_text intro_text--para">
+      <p class="intro_text intro_text--para hidden-start" :class="showText ? 'show' : 'hide'">
         I believe humans are inherently good, but our environments are built in a way that makes us bad. When our environments don't prioritize our wellbeing it creates a disharmoney within ourselves which then goes on to become a negative pattern, we've all experienced road rage and to feel it build. This is proof that all our relationships are connected and we rely on each of them. 
       </p>
 
-      <p class="intro_text intro_text--para">
+      <p class="intro_text intro_text--para hidden-start" :class="showText ? 'show' : 'hide'">
         My Thakurma (Bengali for Father's Mother) came to stay with us from Bangladesh when I was young, she helped me connect with my culture, establishing a sense of wonder and curiosity. When I worked in community mental health as an occupational therapist I began connecting the dots between our wellbeing and urban design. I became so curious that I completed my PhD in cognitive neuroscience to study the psychological impacts of urban design. I learned that urban design is not one size fits all. I learned Kimberle Crenshaw's theory of Intersectionality, that aspects of identity, including race, gender, and sexual orientation, can cause people, and systems to treat people inequitably. 
       </p>
 
-      <p class="intro_text intro_text--para">
+      <p class="intro_text intro_text--para hidden-start" :class="showText ? 'show' : 'hide'">
         Today I propose a conceptual framework called Experiential Equity, which uses neuroscience to measure the disparities in experiences, through looking at physiological indicators like stress and brain activity. It's my mission to heal these disparities, reversing the theory of Intersectionality to create spaces that treat people equitably and thus allow us to bring healthy patterns back into our relationships. 
       </p>
     </div>
@@ -37,7 +37,7 @@ export default  {
   props: [],
   mounted () { },
   data () { return {
-    number_of_leaves: 40,
+    showText: false
   } },
   methods: { },
   computed: { }
@@ -57,6 +57,16 @@ export default  {
     padding: 0;
   }
 
+  &_button {
+    border: none;
+    background-color: var(--brand-colour);
+    color: var(--brand-contrastlight);
+    @media (prefers-color-scheme: dark) {
+      background-color: var(--brand-contrastlight);
+      color: var(--brand-contrastdark);
+    }
+  }
+
   &_text {
     &--title {
       font: var(--bold-text);
@@ -69,10 +79,32 @@ export default  {
 
     &--para {
       line-height: 150%;
-      @media screen and (min-width: 40em) {
-        padding: 2% 0;
+
+      &.hidden-start {
+        transition: all 2s ease-out;
+        &:nth-of-type(2) {
+          transition-delay: .3s;
+        }
+        &:nth-of-type(3) {
+          transition-delay: .6s;
+        }
+        &:nth-of-type(4) {
+          transition-delay: .9s;
+        }
+        opacity: 0;
+        max-height: 0px;
+        &.show {
+          opacity: 1;
+          max-height: 1000px;
+        }
       }
     }
   }
+}
+
+.show {
+  max-height: 100000px;
+}
+.hide {
 }
 </style>
