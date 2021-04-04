657190abb2f293492d6c7a05e41315ce24a8ae9f
Arjun Kalburgi <askalburgi@gmail.com>
2021-04-04 14:45:22 -0400

    form works!

--- src/components/layout/Contact.vue
+++ src/components/layout/Contact.vue
@@ -82,94 +82,82 @@
 </template>
 
 <script lang="js">
-export default  {
-  name: 'contact',
-  methods: {
-    checkForm: function (e) {
-      this.show = false;
+  export default  {
+    name: 'contact',
+    methods: {
+      checkForm: function (e) {
+        this.show = false;
 
-      var form = e.target;
-      var formData = this.getFormData(form);
-      if (formData.honeypot) return;
+        var form = e.target;
+        var formData = this.getFormData(form);
+        if (formData.honeypot) return;
 
-      fetch("https://script.google.com/macros/s/AKfycbwf9vHrFYI6KMJYyhOukPqpd1x1TUhTt41uwqtfmkxUwKmKypGNHmW8cKCdWM9KjAHL/exec", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded"
-        },
-        body: JSON.stringify(formData.data)
-      }).then(() => {
-        console.log("done");
-      })
+        fetch("https://script.google.com/macros/s/AKfycbwf9vHrFYI6KMJYyhOukPqpd1x1TUhTt41uwqtfmkxUwKmKypGNHmW8cKCdWM9KjAHL/exec", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+          },
+          body: formData.data
+        }).then(() => {
+          console.log("Form sent :)");
+        })
+      },
+      getFormData: function(form) {
+        var elements = form.elements;
+        var honeypot;
 
-      // var xhr = new XMLHttpRequest();
-      // xhr.open('POST', "https://script.google.com/macros/s/AKfycbwf9vHrFYI6KMJYyhOukPqpd1x1TUhTt41uwqtfmkxUwKmKypGNHmW8cKCdWM9KjAHL/exec");
-      // // xhr.withCredentials = true;
-      // xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-      // xhr.onreadystatechange = function() {
-      //   if (xhr.readyState === 4 && xhr.status === 200) {
-      //     console.log("complete :)")
-      //   }
-      // };
-      // // url encode form data for sending as post data
-      // var encoded = Object.keys(data).map(function(k) {
-      //     return encodeURIComponent(k) + "=" + encodeURIComponent(data[k]);
-      // }).join('&');
-      // xhr.send(encoded);
-      // console.log("xhr.send(encoded :)")
-    },
-    getFormData: function(form) {
-      var elements = form.elements;
-      var honeypot;
-
-      var fields = Object.keys(elements).filter(function(k) {
-        if (elements[k].name === "honeypot") {
-          honeypot = elements[k].value;
-          return false;
-        }
-        return true;
-      }).map(function(k) {
-        if(elements[k].name !== undefined) {
-          return elements[k].name;
-        // special case for Edge's html collection
-        }else if(elements[k].length > 0){
-          return elements[k].item(0).name;
-        }
-      }).filter(function(item, pos, self) {
-        return self.indexOf(item) == pos && item;
-      });
-
-      var formData = {};
-      fields.forEach(function(name){
-        var element = elements[name];
-
-        // singular form elements just have one value
-        formData[name] = element.value;
-
-        // when our element has multiple items, get their values
-        if (element.length) {
-          var data = [];
-          for (var i = 0; i < element.length; i++) {
-            var item = element.item(i);
-            if (item.checked || item.selected) {
-              data.push(item.value);
-            }
+        var fields = Object.keys(elements).filter(function(k) {
+          if (elements[k].name === "honeypot") {
+            honeypot = elements[k].value;
+            return false;
           }
-          formData[name] = data.join(', ');
-        }
-      });
+          return true;
+        }).map(function(k) {
+          if(elements[k].name !== undefined) {
+            return elements[k].name;
+          // special case for Edge's html collection
+          }else if(elements[k].length > 0){
+            return elements[k].item(0).name;
+          }
+        }).filter(function(item, pos, self) {
+          return self.indexOf(item) == pos && item;
+        });
 
-      return {data: formData, honeypot: honeypot};
-    }
-  },
-  data () {
-    return {
-      show: true,
-      url: 'https://script.google.com/macros/s/AKfycbwf9vHrFYI6KMJYyhOukPqpd1x1TUhTt41uwqtfmkxUwKmKypGNHmW8cKCdWM9KjAHL/exec',
-      honeypot: ''
-    }
-  },
-}
+        var formData = {};
+        fields.forEach(function(name){
+          var element = elements[name];
+
+          // singular form elements just have one value
+          formData[name] = element.value;
+
+          // when our element has multiple items, get their values
+          if (element.length) {
+            var data = [];
+            for (var i = 0; i < element.length; i++) {
+              var item = element.item(i);
+              if (item.checked || item.selected) {
+                data.push(item.value);
+              }
+            }
+            formData[name] = data.join(', ');
+          }
+        });
+
+        var encoded = Object.keys(formData).map(function(k) {
+            return encodeURIComponent(k) + "=" + encodeURIComponent(formData[k]);
+        }).join('&');
+
+        return {data: encoded, honeypot: honeypot};
+      }
+    },
+    data () {
+      return {
+        show: true,
+        url: 'https://script.google.com/macros/s/AKfycbwf9vHrFYI6KMJYyhOukPqpd1x1TUhTt41uwqtfmkxUwKmKypGNHmW8cKCdWM9KjAHL/exec',
+        honeypot: ''
+      }
+    },
+  }
 </script>
 
 <style scoped lang="scss">
